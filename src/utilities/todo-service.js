4708158371a7a3bccdae7db8db62ab76fb6236a5
warrior97 <dbobicic@gmail.com>
2019-11-25 12:46:13 +0100

    BackEnd support

---
+++ src/utilities/todo-service.js
@@ -0,0 +1,32 @@
+
+let apiUrl = 'http://localhost'
+let apiPort = "4001";
+
+
+
+function getToDo(username) {
+    let res = fetch(`${apiUrl}:${apiPort}/api/todo`,{
+        headers: { "Content-Type": "application/json; charset=utf-8" },
+        method: 'POST',
+        body: JSON.stringify({
+          id: username
+        })
+    })
+    .then(response => response.json()
+        , (error) => {
+            console.log(error);
+        })
+    return res;
+}
+function getAllToDo(){
+    let res = fetch(`${apiUrl}:${apiPort}/api/todo`).then(response => response.json()
+        , (error) => {
+            console.log(error);
+        });
+    return res;
+}
+
+export{
+    getToDo,
+    getAllToDo
+}
